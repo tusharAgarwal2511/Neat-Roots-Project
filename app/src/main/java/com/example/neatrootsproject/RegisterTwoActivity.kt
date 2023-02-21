61dd7e2bca4974028f04df4e2341686737099a5e
Sabuj Ahmed <sabujahmed006@gmail.com>
2023-02-21 19:46:40 +0600

    Added login and register screen

---
+++ app/src/main/java/com/example/neatrootsproject/RegisterTwoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.neatrootsproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_two)
+    }
+}
