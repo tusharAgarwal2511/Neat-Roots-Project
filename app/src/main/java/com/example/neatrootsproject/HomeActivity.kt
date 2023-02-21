3df217976cec21fbf0328cd65d5308035f114bd0
Tushar <agarwal25112000@gmail.com>
2023-02-21 13:50:51 +0530

    Added bottom navigation bar with fragments and fixed minor errors

---
+++ app/src/main/java/com/example/neatrootsproject/HomeActivity.kt
@@ -0,0 +1,53 @@
+package com.example.neatrootsproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.size
+import androidx.fragment.app.Fragment
+import com.example.neatrootsproject.Fragments.ChatFragment
+import com.example.neatrootsproject.Fragments.HomeFragment
+import com.example.neatrootsproject.Fragments.ProfileFragment
+import com.example.neatrootsproject.Fragments.SearchFragment
+import com.example.neatrootsproject.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        val homeActivityBinding : ActivityHomeBinding by lazy {
+
+            ActivityHomeBinding.inflate(layoutInflater)
+        }
+
+        setContentView(homeActivityBinding.root)
+
+
+        // default selected item when activity loads
+        homeActivityBinding.bottomNavigationView.setItemSelected(R.id.bottom_navigation_home)
+        replaceFragments(HomeFragment())
+
+        homeActivityBinding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it) {
+
+                R.id.bottom_navigation_home -> replaceFragments(HomeFragment())
+                R.id.bottom_navigation_search -> replaceFragments(SearchFragment())
+                R.id.bottom_navigation_chat -> replaceFragments(ChatFragment())
+                R.id.bottom_navigation_profile -> replaceFragments(ProfileFragment())
+                R.id.bottom_navigation_add -> replaceFragments(HomeFragment())
+
+
+
+            }
+        }
+
+    }
+
+    private fun replaceFragments(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.bottom_navigation_frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
