4be2f1c87f7c7f49f0e41500b6bc417217795521
Sabuj Ahmed <sabujahmed006@gmail.com>
2023-02-21 20:50:10 +0600

    Added register and login activity funtionality

--- app/src/main/java/com/example/neatrootsproject/RegisterOneActivity.kt
+++ app/src/main/java/com/example/neatrootsproject/RegisterOneActivity.kt
@@ -1,11 +1,26 @@
 package com.example.neatrootsproject
 
+
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.neatrootsproject.databinding.ActivityRegisterOneBinding
 
 class RegisterOneActivity : AppCompatActivity() {
+
+    val registerOneActivityBinding by lazy {
+        ActivityRegisterOneBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_one)
+        setContentView(registerOneActivityBinding.root)
+        
+
+        registerOneActivityBinding.btnRegisterNext.setOnClickListener {
+            val intent = Intent(this, RegisterTwoActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/neatrootsproject/RegisterTwoActivity.kt
+++ app/src/main/java/com/example/neatrootsproject/RegisterTwoActivity.kt
@@ -2,10 +2,23 @@ package com.example.neatrootsproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.neatrootsproject.databinding.ActivityRegisterTwoBinding
 
 class RegisterTwoActivity : AppCompatActivity() {
+
+    val registerTwoActivityBinding by lazy {
+        ActivityRegisterTwoBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_two)
+        setContentView(registerTwoActivityBinding.root)
+
+
+
+//       registerTwoActivityBinding.button.setOnClickListener {
+//            val intent = Intent(this, LoginActivity::class:java)
+//            startActivity(intent)
+//            finish()
+//        }
     }
 }
