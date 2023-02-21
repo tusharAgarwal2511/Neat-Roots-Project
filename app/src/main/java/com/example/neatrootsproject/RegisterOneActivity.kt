001c9e3b372e6fb8f1ee9116033ff0dc1eaa2438
Sabuj Ahmed <sabujahmed006@gmail.com>
2023-02-21 20:10:57 +0600

    Created login and register activities

--- app/src/main/java/com/example/neatrootsproject/RegisterOneActivity.kt
+++ app/src/main/java/com/example/neatrootsproject/RegisterOneActivity.kt
@@ -1,11 +1,24 @@
 package com.example.neatrootsproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.neatrootsproject.databinding.ActivityRegisterOneBinding
 
 class RegisterOneActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivityRegisterOneBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_one)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, RegisterTwoActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/neatrootsproject/RegisterTwoActivity.kt
+++ app/src/main/java/com/example/neatrootsproject/RegisterTwoActivity.kt
@@ -1,11 +1,25 @@
 package com.example.neatrootsproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.neatrootsproject.databinding.ActivityRegisterTwoBinding
 
 class RegisterTwoActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityRegisterTwoBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_two)
+
+        supportActionBar!!.hide()
+
+//        binding.button.setOnClickListener {
+//            val intent = Intent(this, LoginActivity::class:java)
+//            startActivity(intent)
+//            finish()
+//        }
     }
 }
