3560b18b7fd044eb8e2f8d2ce449fc3baaa3d10f
Tushar <agarwal25112000@gmail.com>
2023-02-24 11:27:39 +0530

    Added Chat fragment

--- app/src/main/java/com/example/neatrootsproject/fragments/adapters/ChatListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/neatrootsproject/fragments/adapters/ChatListRecyclerViewAdapter.kt
@@ -1,15 +1,36 @@
 package com.example.neatrootsproject.fragments.adapters
 
 
+import android.R
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.RecyclerView
 import com.example.neatrootsproject.databinding.ChatListItemBinding
+import com.example.neatrootsproject.databinding.FragmentChatListBinding
+import com.example.neatrootsproject.fragments.ChatFragment
 import com.example.neatrootsproject.fragments.modals.ChatListModal
 
-class ChatListRecyclerViewAdapter(private val chatList : ArrayList<ChatListModal>) :RecyclerView.Adapter<ChatListRecyclerViewAdapter.ViewHolder>() {
 
-    inner class ViewHolder(val binding : ChatListItemBinding) : RecyclerView.ViewHolder(binding.root){}
+class ChatListRecyclerViewAdapter(private val chatList : ArrayList<ChatListModal>) : RecyclerView.Adapter<ChatListRecyclerViewAdapter.ViewHolder>() {
+
+    private lateinit var chatListBinding : FragmentChatListBinding
+    inner class ViewHolder(val chatListItemBinding : ChatListItemBinding) : RecyclerView.ViewHolder(chatListItemBinding.root){
+
+        init {
+            chatListItemBinding.clChatListItem.setOnClickListener {
+                Toast.makeText(it.context, "Test", Toast.LENGTH_SHORT).show()
+                // Create new fragment
+                
+
+
+
+            }
+
+        }
+    }
 
     override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : ViewHolder {
         val view = ChatListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -25,10 +46,13 @@ class ChatListRecyclerViewAdapter(private val chatList : ArrayList<ChatListModal
 
         val temp = chatList[position] // temporarily storing the current chatList position
 
-        holder.binding.tvChatListName.text = temp.name
-        holder.binding.tvChatListChat.text = temp.chat
-        holder.binding.ivChatListProfile.setImageResource(temp.profile)
+        holder.chatListItemBinding.tvChatListName.text = temp.name
+        holder.chatListItemBinding.tvChatListChat.text = temp.chat
+        holder.chatListItemBinding.ivChatListProfile.setImageResource(temp.profile)
+
 
 
     }
+
+
 }
